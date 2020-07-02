cf53680956fee9da5b51d84d470a017472bea318
DivvyCr <DivvyCr@users.noreply.github.com>
2020-07-02 14:52:49 +0100

    Change the order of operations when installing protoc runtime.

--- _travis/install-protoc-runtime.sh
+++ _travis/install-protoc-runtime.sh
@@ -7,7 +7,12 @@ unzip protobuf-cpp-${protoc_version}.zip
 cd protobuf-cpp-${protoc_version}
 ./configure
 make
-make install
-ldconfig
+
+# Environment variables should be set _prior_ to installing the protobuf library.
+# https://developers.google.com/protocol-buffers/docs/reference/python-generated#cpp_impl
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2
+
+# Install the protobuf library.
+make install
+ldconfig
