836545c04f5de38613ef4919d40ddcc674be9bec
DivvyCr <divvycr@gmail.com>
2020-07-31 14:19:13 +0100

    Final touches?

--- carball/analysis/events/bump_detection/bump_analysis.py
+++ carball/analysis/events/bump_detection/bump_analysis.py
@@ -1,4 +1,5 @@
 import itertools
+import logging
 import numpy as np
 import pandas as pd
 from carball.generated.api.stats.events_pb2 import Bump
@@ -8,13 +9,20 @@ from carball.json_parser.game import Game
 
 from carball.generated.api import game_pb2
 
-# Longest car hitbox is 131.49 (Breakout):
-PLAYER_CONTACT_MAX_DISTANCE = 140
+logger = logging.getLogger(__name__)
+
+# If you decrease this, you risk not counting bumps where one car is directly behind another (driving in the same direction).
+# If you increase this, you risk counting non-contact close proximity (e.g. one car cleanly jumped over another =/= bump).
+PLAYER_CONTACT_MAX_DISTANCE = 200
+
+# Needs to be relatively high to account for two cars colliding 'diagonally': /\
+MAX_BUMP_ALIGN_ANGLE = 60
+
 # Currently arbitrary:
 MIN_BUMP_VELOCITY = 5000
-# Currently arbitrary (degrees):
-MAX_BUMP_ALIGN_ANGLE = 60
-# Approx. half of goal height:
+
+# Approx. half of goal height.
+# (could be used to discard all aerial contact as bumps, although rarely an aerial bump WAS, indeed, intended)
 AERIAL_BUMP_HEIGHT = 300
 
 
@@ -38,13 +46,15 @@ class BumpAnalysis:
         if is_demo:
             bump.is_demo = True
 
-    # def analyse_bumps(self, data_frame: pd.DataFrame):
-
     def create_bumps_from_player_contact(self, data_frame, player_map):
-        print("Entered.")
         # NOTES:
-        #   Contact is not guaranteed (but most likely).
-        #   [WIP] Attacker/Victim is not (yet) determined.
+        # Currently, this yields more 'bumps' than there are actually.
+        #   This is mostly due to aerial proximity, where a bump was NOT intended or no contact was made.
+        #   This also occurs near the ground, where cars flip awkwardly past each other.
+
+        # POSSIBLE SOLUTIONS:
+        #   Account for car hitboxes and/or rotations when calculating close proximity intervals.
+        #   Do some post-bump analysis and see if car velocities aligned (i.e. analyse end of interval bump alignments)
 
         # Get an array of player names to use for player combinations.
         player_names = []
@@ -57,29 +67,37 @@ class BumpAnalysis:
             players_close_frame_idxs = BumpAnalysis.get_players_close_frame_idxs(data_frame,
                                                                                  str(player_pair[0]),
                                                                                  str(player_pair[1]))
-            BumpAnalysis.determine_bumps(data_frame, player_pair, players_close_frame_idxs)
 
-            # if len(players_close_frame_idxs) > 0:
-            #     BumpAnalysis.analyse_bump(data_frame, player_pair, players_close_frame_idxs)
-            # else:
-            #     print("Players did not get close.")
+            if len(players_close_frame_idxs) > 0:
+                BumpAnalysis.analyse_bumps(data_frame, player_pair, players_close_frame_idxs)
+            else:
+                logger.info("Players (" + player_pair[0] + " and " + player_pair[1] + ") did not get close "
+                                                                                      "during the match.")
 
     @staticmethod
-    def filter_consecutive_idxs(players_close_frame_idxs):
-        # THIS SHOULD BE REDONE.
-        # Two problems:
-        #   Probably not best performance with that many loops. (This is the important one.)
-        #   Values are taken from the start/end of time in close distance (bump likely happens in the middle).
+    def analyse_bumps(data_frame, player_pair, players_close_frame_idxs):
+        # Get all individual intervals where a player pair got close to each other.
+        players_close_frame_idxs_intervals = BumpAnalysis.get_players_close_intervals(players_close_frame_idxs)
 
-        # Only append a value, if its diff is >= 3.
-        diffs = np.diff(players_close_frame_idxs)
-        players_close_single_frame_idxs = players_close_frame_idxs[np.append([0], diffs) >= 3]
+        # For each such interval, take (currently only) the beginning and analyse car behaviour.
+        for interval in players_close_frame_idxs_intervals:
+            frame_before_bump = interval[0]
 
-        # If the above didn't append anything (all diffs <= 3), append the first value.
-        if len(players_close_single_frame_idxs) == 0:
-            players_close_single_frame_idxs = np.append(players_close_single_frame_idxs, players_close_frame_idxs[0])
+            # Calculate alignments (angle between position vector and velocity vector, with regard to each player)
+            p1_alignment_before = BumpAnalysis.get_player_bump_alignment(data_frame, frame_before_bump,
+                                                                         player_pair[0], player_pair[1])
+            p2_alignment_before = BumpAnalysis.get_player_bump_alignment(data_frame, frame_before_bump,
+                                                                         player_pair[1], player_pair[0])
+            # TODO Create Bump objects and add them to the API.
+            # Determine the attacker and the victim (if alignment is below MAX_BUMP_ALIGN_ANGLE, it's the attacker)
+            attacker, victim = BumpAnalysis.determine_attacker_victim(player_pair[0], player_pair[1],
+                                                                      p1_alignment_before, p2_alignment_before)
 
-        return players_close_single_frame_idxs
+            # Determine if the bump was above AERIAL_BUMP_HEIGHT.
+            is_aerial_bump = BumpAnalysis.is_aerial_bump(data_frame, player_pair[0], player_pair[1], frame_before_bump)
+
+            # Check if interval is quite long - players may be in rule 1 :) or might be a scramble.
+            BumpAnalysis.analyse_prolonged_proximity(data_frame, interval, player_pair[0], player_pair[1])
 
     @staticmethod
     def get_player_bump_alignment(data_frame, frame_idx, p1_name, p2_name):
@@ -122,107 +140,48 @@ class BumpAnalysis:
 
     @staticmethod
     def get_players_close_intervals(players_close_frame_idxs):
-        array = []
-        subarray = []
-        for index, i in enumerate(players_close_frame_idxs):
+        # Find continuous intervals of close proximity, and group them together.
+        all_intervals = []
+        interval = []
+        for index, frame_idx in enumerate(players_close_frame_idxs):
             diffs = np.diff(players_close_frame_idxs)
-            subarray.append(i)
+            interval.append(frame_idx)
             if index >= len(diffs) or diffs[index] >= 3:
-                array.append(subarray)
-                subarray = []
-        return array
+                all_intervals.append(interval)
+                interval = []
+        return all_intervals
 
     @staticmethod
-    def analyse_bump(data_frame, player_pair, players_close_frame_idxs):
-        # Filter consecutive indexes.
-        players_close_single_frame_idxs = BumpAnalysis.filter_consecutive_idxs(players_close_frame_idxs)
+    def determine_attacker_victim(p1_name, p2_name, p1_alignment, p2_alignment):
+        """
+        Try to 'guesstimate' the attacker and the victim by comparing bump alignment angles.
+            If both alignments are within 45deg, then both players were going relatively towards each other.
 
-        for possible_bump_frame_idx in players_close_single_frame_idxs:
-            # Calculate bump alignments both ways.
-            p1_bump_ang = BumpAnalysis.get_player_bump_alignment(data_frame, possible_bump_frame_idx,
-                                                                 player_pair[0], player_pair[1])
-            p2_bump_ang = BumpAnalysis.get_player_bump_alignment(data_frame, possible_bump_frame_idx,
-                                                                 player_pair[1], player_pair[0])
+        :return: (Attacker, Victim)
+        """
 
-            if p1_bump_ang < 30:
-                s_rem = data_frame.game.seconds_remaining.loc[possible_bump_frame_idx] * -1
-                # if data_frame.game.is_overtime.loc[possible_bump_frame_idx] is None:
-                #     s_rem = s_rem * (-1)
-                bump = (int(s_rem), str(player_pair[0]), str(player_pair[1]))
-                # print(bump)
+        if p1_alignment < MAX_BUMP_ALIGN_ANGLE or p2_alignment < MAX_BUMP_ALIGN_ANGLE:
+            if abs(p1_alignment - p2_alignment) < 45:
+                return None, None
+            elif p1_alignment < p2_alignment:
+                return p1_name, p2_name
+            elif p2_alignment < p1_alignment:
+                return p2_name, p1_name
 
-            if p2_bump_ang < 30:
-                s_rem = data_frame.game.seconds_remaining.loc[possible_bump_frame_idx] * -1
-                # if data_frame.game.is_overtime.loc[possible_bump_frame_idx] is None:
-                #     s_rem = s_rem * (-1)
-                bump = (int(s_rem), str(player_pair[1]), str(player_pair[0]))
-                # print(bump)
+        return None, None
 
     @staticmethod
-    def determine_bumps(data_frame, player_pair, players_close_frame_idxs):
-        players_close_frame_idxs_intervals = BumpAnalysis.get_players_close_intervals(players_close_frame_idxs)
-        for interval in players_close_frame_idxs_intervals:
-            first_frame = -1
-            middle_frame = interval[int(len(interval) / 2)]
-            last_frame = -1
-            if len(interval) > 4:
-                first_frame = interval[0]
-                last_frame = interval[len(interval) - 1]
-
-                p1_bump_ang1 = BumpAnalysis.get_player_bump_alignment(data_frame, first_frame,
-                                                                      player_pair[0], player_pair[1])
-                p1_bump_ang2 = BumpAnalysis.get_player_bump_alignment(data_frame, middle_frame,
-                                                                      player_pair[0], player_pair[1])
-                p1_bump_ang3 = BumpAnalysis.get_player_bump_alignment(data_frame, last_frame,
-                                                                      player_pair[0], player_pair[1])
-                p2_bump_ang1 = BumpAnalysis.get_player_bump_alignment(data_frame, first_frame,
-                                                                      player_pair[1], player_pair[0])
-                p2_bump_ang2 = BumpAnalysis.get_player_bump_alignment(data_frame, middle_frame,
-                                                                      player_pair[1], player_pair[0])
-                p2_bump_ang3 = BumpAnalysis.get_player_bump_alignment(data_frame, last_frame,
-                                                                      player_pair[1], player_pair[0])
-                BumpAnalysis.get_attacker_and_victim(player_pair[0], player_pair[1],
-                                                     np.round(p1_bump_ang1, 2), np.round(p1_bump_ang3, 2),
-                                                     np.round(p2_bump_ang1, 2), np.round(p2_bump_ang3, 2))
-                # print(data_frame.game.seconds_remaining.loc[middle_frame], end=": ")
-                # if BumpAnalysis.is_bump_alignment([p1_bump_ang1, p1_bump_ang2, p2_bump_ang1, p2_bump_ang2]) and BumpAnalysis.is_bump_velocity(data_frame, player_pair[0], player_pair[1], first_frame):
-                #     print("Bump!", end=" ")
-                #
-                # if BumpAnalysis.is_aerial_bump(data_frame, player_pair[0], player_pair[1], first_frame):
-                #     print("(high altitude)", end=" ")
-                # print()
-
-    @staticmethod
-    def get_attacker_and_victim(p1_name, p2_name, p1_ba1, p1_ba3, p2_ba1, p2_ba3):
-        # Basics.
-        print(p1_name, end=": ")
-        print(p1_ba1, end=" > ")
-        print(p1_ba3)
-        print(p2_name, end=": ")
-        print(p2_ba1, end=" > ")
-        print(p2_ba3)
-        # print("===")
-
-        # Angle differences.
-        # print("Initial difference", end=": ")
-        # print(np.round(abs(p1_ba1-p2_ba1), 2))
-        # print("Last difference", end=": ")
-        # print(np.round(abs(p1_ba3-p2_ba3), 2))
-        # print("===")
-
-        # Testing which player had a better initial angle,
-        # then, need to check if the other player's angle has changed towards the primary player's (think momentum).
-        if p1_ba1 < p2_ba1:
-            if p1_ba1 < 45 and p2_ba1 > p2_ba3:
-                print("p1 effectively attacked?")
-            else:
-                print("p1 attacked?")
-        if p2_ba1 < p1_ba1:
-            if p2_ba1 < 45 and p1_ba1 > p1_ba3:
-                print("p2 effectively attacked?")
-            else:
-                print("p2 attacked?")
-        print("===")
+    def analyse_prolonged_proximity(data_frame, interval, p1_name, p2_name):
+        # TODO Redo this to do some proper analysis.
+        if len(interval) > 10:
+            print(" > Scramble between " + p1_name + " and " + p2_name)
+        # NOTE: Could try analysing immediate post-bump effects.
+        # elif len(interval) >= 5:
+        #     frame_after_bump = interval[len(interval) - 1]
+        #     p1_alignment_after = BumpAnalysis.get_player_bump_alignment(data_frame, frame_after_bump,
+        #                                                                 p1_name, p2_name)
+        #     p2_alignment_after = BumpAnalysis.get_player_bump_alignment(data_frame, frame_after_bump,
+        #                                                                 p2_name, p1_name)
 
     @staticmethod
     def is_aerial_bump(data_frame: pd.DataFrame, p1_name: str, p2_name: str, at_frame: int):
